f17259ec03219d6fe8b490f5c722a1c353d86d61
Nick Spinale <nick@nickspinale.com>
2019-03-03 23:36:42 +0000

    READY: gnome3: fix gnome disks

--- pkgs/desktops/gnome-3/core/gnome-disk-utility/default.nix
+++ pkgs/desktops/gnome-3/core/gnome-disk-utility/default.nix
@@ -1,7 +1,7 @@
 { stdenv, gettext, fetchurl, pkgconfig, udisks2, libsecret, libdvdread
 , meson, ninja, gtk3, glib, wrapGAppsHook, python3, libnotify
 , itstool, gnome3, libxml2, gsettings-desktop-schemas
-, libcanberra-gtk3, libxslt, docbook_xsl, libpwquality }:
+, libcanberra-gtk3, libxslt, docbook_xsl, libpwquality, gptfdisk }:
 
 stdenv.mkDerivation rec {
   pname = "gnome-disk-utility";
@@ -20,7 +20,7 @@ stdenv.mkDerivation rec {
   buildInputs = [
     gtk3 glib libsecret libpwquality libnotify libdvdread libcanberra-gtk3
     udisks2 gnome3.adwaita-icon-theme
-    gnome3.gnome-settings-daemon gsettings-desktop-schemas
+    gnome3.gnome-settings-daemon gsettings-desktop-schemas gptfdisk
   ];
 
   postPatch = ''
